04caa80ac8f496e354c27ea8a0b079ef8d873732
unbong <ecunbong@gmail.com>
2023-04-13 09:50:54 +0900

    lab2 实验完成

--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -14,15 +14,13 @@ void TCPReceiver::segment_received(const TCPSegment &seg) {
     TCPHeader header = seg.header();
 
     // syn segment
-    if(header.syn && seg.length_in_sequence_space() == 1)
+    if(header.syn )
     {
         _synFlag = true;
         _isn = header.seqno;
-        _ackNo = 1;
-        _lastReceviedSequence = 1 ;
     }
     // fin segment
-    else if (header.fin ){
+    if (header.fin ){
         _finFlag = true;
         _finSeq = header.seqno;
         // fin syn 同时到达时
@@ -35,36 +33,30 @@ void TCPReceiver::segment_received(const TCPSegment &seg) {
         // fin seq转换未ABS seq checkpoint的值为最后一个组装的字节数
         uint64_t finSeq = unwrap(_finSeq, _isn, _reassembler.stream_out().bytes_written()  );
         // 写入最后一个字符，并指定未EOF
-        _reassembler.push_substring(seg.payload().str().data(),finSeq-1,  EOF_IS_TRUE);
+        _reassembler.push_substring(seg.payload().copy(),finSeq-1,  EOF_IS_TRUE);
         // 因为接收到了fin，ackNo需要占用一个位置，+1 且从流的索引转换到绝对的序列号时也需要+1
-        _ackNo = _reassembler.stream_out().bytes_written()+2;
+
+        _ackNo = _reassembler.stream_out().bytes_written()+1;
+        if(_reassembler.stream_out().input_ended())
+            _ackNo++;
 
     }
     // 已经建立连接的前提下
-    else
+    else if(_synFlag)
     {
-        if (header.syn  ) {
-            _synFlag = true;
-            _isn = header.seqno;
 
-        }
-        //_synFlag = true;
-        _isn = header.seqno;
-
-        // sequence -> abs sequence
         uint64_t absSeq = unwrap(header.seqno, _isn, _reassembler.stream_out().bytes_written() );
         // 写入StreamReAssembler中
         Buffer payload = seg.payload();
-        //
-        _reassembler.push_substring(payload.str().data(), absSeq-1, !EOF_IS_TRUE);
+        // 如果时syn和数据一起收到时数据的index需要加1
+        if(absSeq == 0 && header.syn )
+            absSeq++;
 
+        _reassembler.push_substring(payload.copy(), absSeq-1, !EOF_IS_TRUE);
         _ackNo = _reassembler.stream_out().bytes_written()+1;
-
-        if(_lastReceviedSequence < absSeq + payload.size()   )
-        {
-            _lastReceviedSequence = absSeq + payload.size()  ;
-        }
-
+        // 输入结束时ack需要加1
+        if(_reassembler.stream_out().input_ended())
+            _ackNo++;
     }
 }
 
